9517f2004e6f69f443fbad88bcd0e4898317387a
jvjayaraj4 <113084404+JVJayarah3@users.noreply.github.com>
2023-06-11 13:29:26 +0530

    Update casing_details.py

--- pages/casing_details.py
+++ pages/casing_details.py
@@ -1,1 +1,8 @@
 
+with st.container():
+  st.title("CASING - DETAILS")
+  od = st.number_input("ENTER THE OD OF CASING",)
+  id = st.number_input("ENTER THE ID OF CASING",)
+  casing_cap = ((id**2)/1029.4)
+button1  = st.button("SUBMIT")
+shoetrack = casing_cap*(csd-fc)
