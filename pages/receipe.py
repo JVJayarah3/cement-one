9eef1bbde649c9fe648e73c3136579898f91462b
jvjayaraj4 <113084404+JVJayarah3@users.noreply.github.com>
2023-06-11 15:16:07 +0530

    Update receipe.py

--- pages/receipe.py
+++ pages/receipe.py
@@ -9,7 +9,7 @@ with st.container():
   st.header("TOTAL VOLUME OF SLURRY NEEDED - "+str(total_slurry_needed))
   cementyld = st.number_input("CEMENT YIELD (CF-SK) - ")  
   cement_sk = (total_slurryneeded/(cementyld*0.178))         
-  st.header("CEMENT-RECEIPE)
+  st.header("CEMENT-RECEIPE")
   col1,col2,col3 = st.columns(3):
   with col1:
     name1 = st.text_input("RECEIPE NAME - ")
