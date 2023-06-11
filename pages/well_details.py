ea842a235517a1fb2e40204769c15279c4034d65
jvjayaraj4 <113084404+JVJayarah3@users.noreply.github.com>
2023-06-11 13:26:21 +0530

    Update well_details.py

--- pages/well_details.py
+++ pages/well_details.py
@@ -1,1 +1,18 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Test")
+with st.container():
+  st.title("WELL-DETAILS")
+  tvd = st.number_input("ENTER THE TVD (ft)",)
+  md = st.number_input("ENTER THE MD  (ft)",)
+  csd = st.number_input("ENTER THE SHOE DEPTH  (ft)",)
+  fc = st.number_input("ENTER THE F/C DEPTH  (ft)",)
+  toc = st.number_input("ENTER THE TOC  (ft)",)
+  excess_cement = st.number_input("Excess Volume Pecentage (normally 20%)",)
+  holesize = st.number_input("ENTER THE AVERAGE HOLE SIZE (IN)",)
+st.write("----------------------------------------------------")
+submit1 = st.button("SUBMIT")
+if submit1:
+    
 
