0ae5746f3920600eccc27bdbc416180952ef4627
jvjayaraj4 <113084404+JVJayarah3@users.noreply.github.com>
2023-06-11 19:09:26 +0530

    Update output.py

--- pages/output.py
+++ pages/output.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from streamlit_extras.switch_page_button import switch_page
 import numpy as np
+
 #total_slurry_needed = st.session_state['total_slurry_need'] 
 #pump_out = st.session_state['pump_out']
 #displacement_fluid = st.session_state['displacement_fluid']
@@ -17,6 +18,12 @@ import numpy as np
 #well_name = st.session_state['well_name']
 #date = st.session_state['date']
 #dead_vol = st.session_state['dead_vol']
+#bump_p = st.session_state['bump_p']
+#cement_den = st.session_state['cement_den']
+#cement_sk = st.session_state['cement_sk']
+#preflush = st.session_state['preflush']
+#woc = st.session_state['woc']
+
 
 st.title(str(well_name) + " - "+ str(od)+"INCH CEMENTING PROGRAMME")
 st.header("OBJECTIVE : TO CEMENT "+str(od)+" INCH CASING/LINER")
