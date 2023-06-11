6c3cc47d8fb464fdfc64522ca317b6331c8db07e
jvjayaraj4 <113084404+JVJayarah3@users.noreply.github.com>
2023-06-11 13:47:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ import streamlit as st
 import pandas as pd
 from streamlit_extras.switch_page_button import switch_page
 st.text("Test")
-butn = st.button("SUBMIT")
-if butn:
-  switch_page("well_details")
+
+
+with st.container():
+  st.title("CASING - DETAILS")
+  od = st.number_input("ENTER THE OD OF CASING",)
+  id = st.number_input("ENTER THE ID OF CASING",)
+  casing_cap = ((id**2)/1029.4)
+
+shoetrack = casing_cap*(csd-fc)
 
  
