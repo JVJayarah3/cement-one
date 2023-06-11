af398dd331f5bf43f685966677b297b8855b85f9
jvjayaraj4 <113084404+JVJayarah3@users.noreply.github.com>
2023-06-11 13:18:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ with st.container():
   st.title("CASING - DETAILS")
   od = st.number_input("ENTER THE OD OF CASING",)
   id = st.number_input("ENTER THE ID OF CASING",)
-  casing_cap = ((id^2)/1029.4)
+  casing_cap = ((id**2)/1029.4)
 button1  = st.button("SUBMIT")
 shoetrack = casing_cap*(csd-fc)
 if button1:
