e2abb2e109570c287db34d566a2a7ff68b0b5d3e
jvjayaraj4 <113084404+JVJayarah3@users.noreply.github.com>
2023-06-11 14:31:49 +0530

    Update receipe.py

--- pages/receipe.py
+++ pages/receipe.py
@@ -1,1 +1,9 @@
+import streamlit as st
+import pandas as pd
+from streamlit_extras.switch_page_button import switch_page
+st.text("Test")
 
+
+with st.container():
+  st.title("TOTAL VOLUME OF SLURRY NEEDED - ")
+  
