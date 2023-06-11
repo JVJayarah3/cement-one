c2ced2201a2a99593e5c5c8e957cb251717c354b
jvjayaraj4 <113084404+JVJayarah3@users.noreply.github.com>
2023-06-11 13:29:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,6 @@ import pandas as pd
 
 st.text("Test")
 with st.container():
-  st.title("WELL-DETAILS")
-  tvd = st.number_input("ENTER THE TVD (ft)",)
-  md = st.number_input("ENTER THE MD  (ft)",)
-  csd = st.number_input("ENTER THE SHOE DEPTH  (ft)",)
-  fc = st.number_input("ENTER THE F/C DEPTH  (ft)",)
-  toc = st.number_input("ENTER THE TOC  (ft)",)
-  excess_cement = st.number_input("Excess Volume Pecentage (normally 20%)",)
-  holesize = st.number_input("ENTER THE AVERAGE HOLE SIZE (IN)",)
-st.write("----------------------------------------------------")
-with st.container():
   st.title("CASING - DETAILS")
   od = st.number_input("ENTER THE OD OF CASING",)
   id = st.number_input("ENTER THE ID OF CASING",)
