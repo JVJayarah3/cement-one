cbf8e582ed4d2e09681d6b022e9fba743ed500d1
jvjayaraj4 <113084404+JVJayarah3@users.noreply.github.com>
2023-06-11 18:24:00 +0530

    Update receipe.py

--- pages/receipe.py
+++ pages/receipe.py
@@ -16,7 +16,7 @@ od = 5.5
 csd = 5156
 well_name = "BK#32"
 date = "11-06-2023"
-
+md = 5167
 #total_slurry_needed = st.session_state['total_slurry_need'] 
 #pump_out = st.session_state['pump_out']
 #displacement_fluid = st.session_state['displacement_fluid']
