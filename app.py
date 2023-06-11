af1036c78f94ad75ef97f921ea9e2fa7d9bc8fa8
jvjayaraj4 <113084404+JVJayarah3@users.noreply.github.com>
2023-06-11 13:31:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,8 @@ import streamlit as st
 import pandas as pd
 
 st.text("Test")
-with st.container():
-  st.title("CASING - DETAILS")
-  od = st.number_input("ENTER THE OD OF CASING",)
-  id = st.number_input("ENTER THE ID OF CASING",)
-  casing_cap = ((id**2)/1029.4)
-button1  = st.button("SUBMIT")
-shoetrack = casing_cap*(csd-fc)
-if button1:
-  st.write("CEMENT VOLUME IN SHOE TRACK"+str(shoetrack)+"bbls")
-  
+butn = st.button("SUBMIT")
+if butn:
+  switch_page("well_details")
+
  
