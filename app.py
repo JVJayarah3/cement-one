25dce22dcba41d408a8dc131074f50968c75f314
jvjayaraj4 <113084404+JVJayarah3@users.noreply.github.com>
2023-06-11 13:48:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,16 @@ import pandas as pd
 from streamlit_extras.switch_page_button import switch_page
 st.text("Test")
 
+with st.container():
+  st.title("WELL-DETAILS")
+  tvd = st.number_input("ENTER THE TVD (ft)",)
+  md = st.number_input("ENTER THE MD  (ft)",)
+  csd = st.number_input("ENTER THE SHOE DEPTH  (ft)",)
+  fc = st.number_input("ENTER THE F/C DEPTH  (ft)",)
+  toc = st.number_input("ENTER THE TOC  (ft)",)
+  excess_cement = st.number_input("Excess Volume Pecentage (normally 20%)",)
+  holesize = st.number_input("ENTER THE AVERAGE HOLE SIZE (IN)",)
+st.write("----------------------------------------------------")
 
 with st.container():
   st.title("CASING - DETAILS")
@@ -12,4 +22,5 @@ with st.container():
 
 shoetrack = casing_cap*(csd-fc)
 
+st.write("----------------------------------------------------")
  
