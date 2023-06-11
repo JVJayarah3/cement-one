279d7c79f3dd8404a85238e82f1798cf1e41add1
jvjayaraj4 <113084404+JVJayarah3@users.noreply.github.com>
2023-06-11 14:32:48 +0530

    Update receipe.py

--- pages/receipe.py
+++ pages/receipe.py
@@ -3,7 +3,7 @@ import pandas as pd
 from streamlit_extras.switch_page_button import switch_page
 st.text("Test")
 
-
+total_slurry_needed = st.session_state['total_slurry_need'] 
 with st.container():
   st.title("TOTAL VOLUME OF SLURRY NEEDED - ")
   
